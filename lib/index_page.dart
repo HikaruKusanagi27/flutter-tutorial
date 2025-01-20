93ef8a1edfe51ffa9735f0aa8f383f7f8e6c89fc
HikaruKusanagi <hikarugp24@gmail.com>
2025-01-20 14:04:19 +0900

    fix: コミットまとめ

---
+++ lib/index_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:test/building/building_layout_page.dart';
+import 'package:test/residence/residence_page.dart';
+import 'package:test/animation/animation_page.dart';
+
+class IndexPage extends StatelessWidget {
+  const IndexPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => BuildingLayoutPage()),
+                  );
+                },
+                child: Text('Tutorial1')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AnimationPage()),
+                  );
+                },
+                child: Text('Tutorial1-1')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResidencePage()),
+                  );
+                },
+                child: Text('2.2 Residence')),
+          ],
+        ),
+      ),
+    );
+  }
+}
