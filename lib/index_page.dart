c09547d562c3cf2889f33209b402ac4969298e16
HikaruKusanagi <hikarugp24@gmail.com>
2025-01-23 12:50:05 +0900

    feat: 静的解析の導入

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -13,22 +13,27 @@ class IndexPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => BuildingLayoutPage()),
-                  );
-                },
-                child: Text('Tutorial1')),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<BuildingLayoutPage>(
+                    builder: (context) => const BuildingLayoutPage(),
+                  ),
+                );
+              },
+              child: const Text('Tutorial1'),
+            ),
             ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AnimationPage()),
-                  );
-                },
-                child: Text('Tutorial1-1')),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<AnimationPage>(
+                    builder: (context) => const AnimationPage(),
+                  ),
+                );
+              },
+              child: const Text('Tutorial1-1'),
+            ),
           ],
         ),
       ),
