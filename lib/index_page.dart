a2f2e1b177615834dd5345ba1f40ad2e04806d86
HikaruKusanagi <hikarugp24@gmail.com>
2025-01-31 13:57:51 +0900

    feat: MVVMを学び、実装する

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test/animation/animation_page.dart';
 import 'package:test/building/building_layout_page.dart';
+import 'package:test/qiita/ui/qiita_page.dart';
 import 'package:test/residence/residence_page.dart';
 import 'package:test/merucari/merucari_page.dart';
 import 'package:test/youtube/youtube_page.dart';
@@ -69,6 +70,17 @@ class IndexPage extends StatelessWidget {
                 );
               },
               child: const Text('2.3 mercari'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<QiitaPage>(
+                    builder: (context) => const QiitaPage(),
+                  ),
+                );
+              },
+              child: const Text('Tutorial6 MVVM'),
             ),
           ],
         ),
---
+++ lib/qiita/repository/qiita_repository.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'qiita_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$qiitaRepositoryHash() => r'300c200aaec60a2d3ed83979ed7e255b71bc8ab4';
+
+/// See also [qiitaRepository].
+@ProviderFor(qiitaRepository)
+final qiitaRepositoryProvider = AutoDisposeProvider<QiitaRepository>.internal(
+  qiitaRepository,
+  name: r'qiitaRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$qiitaRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef QiitaRepositoryRef = AutoDisposeProviderRef<QiitaRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
